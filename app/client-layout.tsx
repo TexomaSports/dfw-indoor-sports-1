10c205be2ba3c80b714e1306811fb51e586c5a3a
codexstar69 <engazedigital@gmail.com>
2025-12-14 05:04:27 +0530

    Auto-sync after agent response

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -3,6 +3,7 @@
 import { ThemeProvider } from '@/context/ThemeContext'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import BackToTop from '@/components/BackToTop'
 
 export default function ClientLayout({
   children,
@@ -15,6 +16,7 @@ export default function ClientLayout({
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
+        <BackToTop />
       </div>
     </ThemeProvider>
   )
